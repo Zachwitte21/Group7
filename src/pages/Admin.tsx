37439ec63fea066dc03cd76e623289fe3c0a030a
seanhnsy <124756688+seanhnsy@users.noreply.github.com>
2023-05-14 12:12:21 -0400

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -27,6 +27,7 @@ function AdminPage(): JSX.Element {
                 {storeItems.map((item) => (
                     <Col key={item.id}>
                         <StoreItem {...item} />
+                        {item.id}
                     </Col>
                 ))}
             </Row>
